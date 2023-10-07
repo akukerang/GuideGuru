13b0ab4aa7d1bc3a9d68aa7900cc813cd451418c
Gabriel Suoth <gabrielsuoth@gmail.com>
2023-10-07 04:40:05 -0400

    Google Map API Implementation

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-
+import config
+api_key = config.API_KEY
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return render_template('index.html')
+def main():
+    return render_template('index.html',api_key = api_key)
+
+
 
 app.run()

fb8f0f4afa8a5a578a5c309fce854113722505f8
Gabriel Suoth <gabrielsuoth@gmail.com>
2023-10-07 20:47:14 -0400

    result page functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import keys
 import openai
 from googlemaps import places, Client
+import pickle
 google = keys.google
 app = Flask(__name__, static_url_path='/static')
-openai = keys.openai
+openai.api_key = keys.openaiKey
+app.secret_key = 'your_secret_key'  
 
 def getQuery(occassion, interests, budget):
     query = f"{occassion} occassian with interests in {interests} with a ${budget} budget near me"
@@ -28,10 +30,10 @@ def locations():
     longitude = request.args.get('longitude')
     latitude = request.args.get('latitude')
     radius = request.args.get('radius')
-    occassion = request.args.get('occassion')
+    occassion = request.args.get('occassian')
     interests = request.args.get('interests')
     budget = request.args.get('budget')
-
+    session['query'] = getQuery(occassion, interests, budget)
     interests = interests.split(", ")
     gmaps = Client(key=google)
 
@@ -58,13 +60,53 @@ def locations():
                 }});
                 markers.push(marker);
             """
-
+    save_data_to_file(locationList)
     return render_template('locations.html', locationList = locationList, len=len(locationList), api_key = google, markers=markers)
 
-@app.route('/results')
+@app.route('/results/')
 def results():
-    return render_template('results.html')
-    
+    locationList = load_data_from_file()
+    chosen_locations = []
+    index_array = request.args.get('indexes')
+    if index_array:
+        index_array = list(index_array.split(','))
+        for i in index_array:
+            chosen = {
+                "name": locationList[int(i)]['name'],
+                "tags": locationList[int(i)]['type'],
+                "price": locationList[int(i)]['price_level'],
+            }
+            chosen_locations.append(chosen)
+    response = planDay(session.get('query'), chosen_locations)
+    return render_template('results.html', response=response)
+
+
+def load_data_from_file():
+    try:
+        with open('location_data.pkl', 'rb') as file:
+            location_dict = pickle.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist yet, return an empty dictionary
+        location_dict = {}
+    return location_dict
+
+def save_data_to_file(location_dict):
+    with open('location_data.pkl', 'wb') as file:
+        pickle.dump(location_dict, file)
+
+
+def planDay(query, locations):
+    prompt = "Plan me a day where I have a "+query+" and these are my list of locations" + str(locations)
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo", 
+    max_tokens=200,
+    messages = [
+        {"role": "system", "content" : "You give a plan for a day with an occassian and a list of locations."},
+        {"role": "user", "content" :  prompt},
+    ]
+    )
+    return response["choices"][0]["message"]["content"]
+
 
 app.run()
 
